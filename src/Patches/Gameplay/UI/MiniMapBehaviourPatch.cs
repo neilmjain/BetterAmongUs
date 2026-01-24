ad3898817e99a5113f5821c91518094bbbbe35c0
D1GQ <yourboylax@gmail.com>
2026-01-23 19:59:59 -0600

    Recolor detective map

--- src/Patches/Gameplay/UI/MiniMapBehaviourPatch.cs
+++ src/Patches/Gameplay/UI/MiniMapBehaviourPatch.cs
@@ -12,9 +12,15 @@ internal static class MiniMapBehaviourPatch
         [HarmonyPatch(nameof(MapBehaviour.ShowNormalMap))]
         [HarmonyPostfix]
         private static void ShowNormalMap_Postfix(MapBehaviour __instance) => __instance.ColorControl.SetColor(new Color(0.05f, 0.6f, 1f, 1f));
+
+        [HarmonyPatch(nameof(MapBehaviour.ShowDetectiveMap))]
+        [HarmonyPostfix]
+        private static void ShowDetectiveMap_Postfix(MapBehaviour __instance) => __instance.ColorControl.SetColor(new Color(0.05f, 0.6f, 1f, 1f));
+
         [HarmonyPatch(nameof(MapBehaviour.ShowSabotageMap))]
         [HarmonyPostfix]
         private static void ShowSabotageMap_Postfix(MapBehaviour __instance) => __instance.ColorControl.SetColor(new Color(1f, 0.3f, 0f, 1f));
+
         [HarmonyPatch(nameof(MapBehaviour.ShowCountOverlay))]
         [HarmonyPostfix]
         private static void ShowCountOverlay_Postfix(MapBehaviour __instance) => __instance.ColorControl.SetColor(new Color(0.2f, 0.5f, 0f, 1f));
