60c8b035ff2426a4dbf1e8c2abafaed6b9e146fc
D1GQ <yourboylax@gmail.com>
2026-01-29 16:47:56 -0600

    add ForceSkipCommand

---
+++ src/Commands/ForceSkipCommand.cs
@@ -0,0 +1,23 @@
+﻿using BetterAmongUs.Attributes;
+using BetterAmongUs.Modules;
+
+namespace BetterAmongUs.Commands;
+
+[RegisterCommand]
+internal sealed class ForceSkipCommand : BaseCommand
+{
+    internal override string Name => "forceskip";
+    internal override string Description => "Force skips a meeting in progress";
+    internal override bool ShowCommand() => GameState.IsHost && GameState.IsMeeting;
+    internal override void Run()
+    {
+        if (GameState.IsHost)
+        {
+            foreach (var client in AmongUsClient.Instance.allClients)
+            {
+                MeetingHud.Instance.RpcClearVote(client.Id);
+            }
+            MeetingHud.Instance.Close();
+        }
+    }
+}
